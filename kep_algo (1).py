20d19e0022f0b45d4860a181bbdf34d3f325286a
sismo93 <wassim.kezai93@gmail.com>
2021-03-12 20:48:21 -0100

    upload

---
+++ kep_algo (1).py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Aliaksei Vainilovich
+# ID: insert ulb id (000492918)
+
+from networkx import DiGraph, simple_cycles
+
+# naming convention
+patient = lambda i: "p_{{{}}}".format(i)
+donor = lambda i: "d_{{{}}}".format(i)
+
+
+# insert any auxiliary code
+def read_csv(file):
+    with open(file, 'r') as f:
+        row = f.readline().split(';')
+        number_of_patient_donor_pairs = int(row[0])
+        number_of_compatibilities = int(row[1])
+        maximum_number_of_exchanges = int(row[2])
+
+        di_pj_arcs = []
+        for _ in range(number_of_compatibilities):
+            row = f.readline().split(';')
+            di_pj_arcs.append([donor(row[0]), patient(row[1]), float(row[2])])
+
+        return number_of_patient_donor_pairs, number_of_compatibilities, maximum_number_of_exchanges, di_pj_arcs
+
+
+def get_cycle_weight(nodes, weights_dict):
+    result = 0.0
+    current = nodes[-1]
+
+    for next_node in nodes:
+        result += weights_dict[(current, next_node)]
+        current = next_node
+
+    return round(result, 9)
+
+
+def solve(args):
+    number_of_patient_donor_pairs, _, _, di_pj_arcs = read_csv(args.instance)
+
+    pi_di_arcs = [[patient(i), donor(i), 0.0] for i in range(number_of_patient_donor_pairs)]
+
+    arcs_cost = dict((tuple(arc[:2]), arc[2]) for arc in di_pj_arcs + pi_di_arcs)
+    X_dict = dict((tuple(arc[:2]), 0) for arc in di_pj_arcs + pi_di_arcs)
+
+    while True:
+        forward_arcs_set = set((pair[0] for pair in X_dict.items() if pair[1] == 0))
+        backward_arcs_set = set((pair[0][::-1] for pair in X_dict.items() if pair[1] == 1))
+        backward_arcs_set -= forward_arcs_set.intersection(backward_arcs_set)
+
+        residual_cost_data = dict(((arc, arcs_cost[arc]) for arc in forward_arcs_set))
+        residual_cost_data.update(dict(((arc, -1 * arcs_cost[arc[::-1]]) for arc in backward_arcs_set)))
+
+        cycles_nodes = simple_cycles(DiGraph(iter(residual_cost_data.keys())))
+
+        for cycle_nodes in cycles_nodes:
+            cycle_weight = get_cycle_weight(cycle_nodes, residual_cost_data)
+
+            if cycle_weight > 0:
+                cycle_arcs = list(zip(cycle_nodes, cycle_nodes[1:] + cycle_nodes[:1]))
+
+                forward_arcs_to_update = forward_arcs_set.intersection(cycle_arcs)
+                backward_arcs_to_update = backward_arcs_set.intersection(cycle_arcs)
+
+                for arc in forward_arcs_to_update:
+                    X_dict[arc] += 1
+
+                for arc in backward_arcs_to_update:
+                    X_dict[arc[::-1]] -= 1
+
+                break
+
+        else:
+            break
+
+    return [pair[0] for pair in X_dict.items() if pair[1] == 1]
---
+++ kep_ip (1).py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Aliaksei Vainilovich
+# ID: insert ULB id (000492918)
+
+from pyomo.environ import *
+
+# naming convention
+patient = lambda i: "p_{{{}}}".format(i)
+donor = lambda i: "d_{{{}}}".format(i)
+
+
+# insert any auxiliary code
+def read_csv(file):
+    with open(file, 'r') as f:
+        row = f.readline().split(';')
+        number_of_patient_donor_pairs = int(row[0])
+        number_of_compatibilities = int(row[1])
+        maximum_number_of_exchanges = int(row[2])
+
+        di_pj_arcs = []
+        for _ in range(number_of_compatibilities):
+            row = f.readline().split(';')
+            di_pj_arcs.append([donor(row[0]), patient(row[1]), float(row[2])])
+
+        return number_of_patient_donor_pairs, number_of_compatibilities, maximum_number_of_exchanges, di_pj_arcs
+
+
+def find_out_of_limit_cycles(arcs, cycle_limit):
+    source = set([arc[0] for arc in arcs])
+    target_dict = dict(arcs)
+    result = []
+
+    while source:
+        start = source.pop()
+        cycle = [start]
+        node = target_dict[start]
+
+        while node != start:
+            cycle.append(node)
+            node = target_dict[node]
+
+        if len(cycle) > cycle_limit:
+            result.append(cycle)
+
+        source -= set(cycle)
+
+    return result
+
+
+def create_model(nodes_set, arcs_set, arcs_cost, cycle_limit):
+    model = AbstractModel()
+
+    model.node_set = Set(initialize=nodes_set)
+    model.arc_set = Set(initialize=arcs_set, dimen=2)
+
+    model.X = Var(model.arc_set, domain=Binary if cycle_limit else PercentFraction)
+
+    def obj_expression(_model):
+        return sum(_model.X[arc] * arcs_cost[arc] for arc in arcs_set)
+
+    model.OBJ = Objective(rule=obj_expression, sense=maximize)
+
+    def constraint_rule(_model, node):
+        source = (arc[0] for arc in arcs_set if arc[1] == node)
+        target = (arc[1] for arc in arcs_set if arc[0] == node)
+
+        return sum(_model.X[(node, t)] for t in target) - sum(_model.X[(s, node)] for s in source) == 0
+
+    model.Constraint1 = Constraint(model.node_set, rule=constraint_rule)
+
+    if cycle_limit:
+        model.cycle_pool = Set(dimen=1)
+
+        def cycle_constraint_rule(_model, cycle):
+            return sum(_model.X[arc] for arc in cycle) <= (len(cycle) - 1)
+
+        model.Constraint2 = Constraint(model.cycle_pool, rule=cycle_constraint_rule)
+
+    return model
+
+
+def solve(args):
+    opt = SolverFactory('glpk')
+    number_of_patient_donor_pairs, _, maximum_number_of_exchanges, di_pj_arcs = read_csv(args.instance)
+    cycle_limit = maximum_number_of_exchanges * 2 if args.cycle_limit else 0
+
+    pi_di_arcs = [[patient(i), donor(i), 0.0] for i in range(number_of_patient_donor_pairs)]
+
+    arcs_cost = dict((tuple(arc[:2]), arc[2]) for arc in di_pj_arcs + pi_di_arcs)
+
+    nodes_set = set([donor(i) for i in range(number_of_patient_donor_pairs)]
+                    + [patient(i) for i in range(number_of_patient_donor_pairs)])
+
+    arcs_set = set(arcs_cost.keys())
+
+    model = create_model(nodes_set, arcs_set, arcs_cost, cycle_limit)
+
+    while True:
+        instance = model.create_instance()
+        opt.solve(instance)
+
+        if not cycle_limit:
+            break
+
+        limit_cycles = find_out_of_limit_cycles([arc for arc in instance.X if value(instance.X[arc])], cycle_limit)
+
+        if not limit_cycles:
+            break
+
+        for elem in [frozenset(zip(cycle, cycle[1:] + cycle[:1])) for cycle in limit_cycles]:
+            model.cycle_pool.add(elem)
+
+    return [arc for arc in instance.X if value(instance.X[arc])]
